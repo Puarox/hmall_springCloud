a77fb48f020cbf1ebf0be6627c651812dc171abf
蔺宇辉 <2207619182@qq.com>
2025-08-30 16:19:41 +0800

    添加了基于GlobalFilter和拦截器的用户校验以及信息保存。引入了Sentinel进行服务保护

---
+++ hmall/hm-common/src/main/java/com/hmall/common/interceptors/UserInfoInterceptor.java
@@ -0,0 +1,27 @@
+package com.hmall.common.interceptors;
+
+import cn.hutool.core.util.StrUtil;
+import com.hmall.common.utils.UserContext;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserInfoInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1 get the info
+        String userInfo = request.getHeader("user-info");
+        //2 judge if info is empty
+        if(StrUtil.isNotBlank(userInfo)) {
+            UserContext.setUser(Long.valueOf(userInfo));
+        }
+        //3
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserContext.removeUser();
+    }
+}
